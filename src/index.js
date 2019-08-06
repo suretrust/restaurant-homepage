e7cc3c7d83a511de8eb3c467bcdfa4607e62ded9
Saheed <sdrfngg640@gmail.com>
2019-08-06 19:10:45 +0100

    Add index JS file

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import pageLoad from './pageLoad';
+import menu from './menuContent';
+import contact from './contactContent';
+
+pageLoad.getContent();
+
+let content = document.getElementById("content");
+
+document.addEventListener("click", function(e){
+    if (e.target.id == "menu") {
+        content.removeChild(contact.content);
+        content.appendChild(menu.content);
+    } else if (e.target.id == "contact") {
+        content.removeChild(menu.content);
+        content.appendChild(contact.content);
+    }
+});
