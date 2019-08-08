e5f30f84ce20ef08234be9233a1ee93bb7876fec
Saheed <sdrfngg640@gmail.com>
2019-08-08 15:13:00 +0100

    Add build and dev webpack commands

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ pageLoad.getContent();
 let content = document.getElementById("content");
 
 document.addEventListener("click", function(e){
-    if (e.target.id == "menu") {
-        if (document.contains(contact.content)) {
-            content.removeChild(contact.content);
-            content.appendChild(menu.content);
-        }
+    if ((e.target.id == "menu") && (document.contains(contact.content))) {
+        content.removeChild(contact.content);
+        content.appendChild(menu.content);
     } else if (e.target.id == "contact") {
         content.removeChild(menu.content);
         content.appendChild(contact.content);
