d93706376fe35e9f4f0c0c5b6842ca8442480324
Saheed <sdrfngg640@gmail.com>
2019-08-08 09:48:33 +0100

    Fix child does not exist  bug on "Our Menu" first click

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ let content = document.getElementById("content");
 
 document.addEventListener("click", function(e){
     if (e.target.id == "menu") {
-        content.removeChild(contact.content);
-        content.appendChild(menu.content);
+        if (document.contains(contact.content)) {
+            content.removeChild(contact.content);
+            content.appendChild(menu.content);
+        }
     } else if (e.target.id == "contact") {
         content.removeChild(menu.content);
         content.appendChild(contact.content);
